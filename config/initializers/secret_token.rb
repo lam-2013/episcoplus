5996359d9ae07058a3f0f1f8d22e42c265410fb7
pasqLisena <pasq.lisena@gmail.com>
2013-05-31 15:21:25 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Episcoplus::Application.config.secret_token = '5af746754a1322adeb7c2b503ea4cc70ce113ec25113d659ba85a2acb4e607cd2bb1cfdde4aacce2c29114864f1692169346c0154353fe04814a7c7fbf36b139'
