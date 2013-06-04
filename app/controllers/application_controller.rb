6f08776c606cbb68549223f225eaae43b2b521f5
pasqLisena <pasq.lisena@gmail.com>
2013-06-04 22:44:08 +0200

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+
+  # Force signout to prevent CSRF attacks
+  def handle_unverified_request
+    sign_out
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  # Return a title on a per-page basis
+  def full_title(page_title)
+    base_title = 'episco+'
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+  # Return a title on a per-page basis
+  def preLogin?(isPreLogin)
+    if isPreLogin.empty?
+       false
+    else
+      isPreLogin
+    end
+
+  end
 end
