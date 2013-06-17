4fa6d504288867c44a31f6fc93318218fffed5ac
Mariella Sabatino <mll.sabatino@gmail.com>
2013-06-17 22:58:11 +0200

    fix collisione routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Episcoplus::Application.routes.draw do
   get "like/destroy"
 
   get 'tags/:tag', to: 'sermon#show', as: :tag
-  resources :sermons
-  root :to => 'sermons#show'
 
   # route for the homepage
   root :to => 'pages#home'
